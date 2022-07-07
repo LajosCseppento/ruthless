67940b1ada5fca28fe26ef50967f663a85dfd793
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2022-07-08 01:42:27 +0200

    Update demo

---
+++ ruthless-demo/build.gradle.kts
@@ -0,0 +1,26 @@
+// For testing Ruthless Logging
+tasks.register("fail") {
+    doLast {
+        throw GradleException("Failing the build", IllegalStateException("Failure cause"))
+    }
+}
+
+allprojects {
+    tasks.register("log") {
+        doLast {
+            println("Hello via println!")
+
+            logger.error("Error log")
+            logger.error(
+                "[error] Error log with exception",
+                GradleException("Logged exception", IllegalStateException("Logged exception cause"))
+            )
+            logger.quiet("Quiet log")
+            logger.warn("Warn log")
+            logger.lifecycle("Lifecycle log")
+            logger.info("Info log")
+            logger.debug("Debug log")
+        }
+    }
+
+}
