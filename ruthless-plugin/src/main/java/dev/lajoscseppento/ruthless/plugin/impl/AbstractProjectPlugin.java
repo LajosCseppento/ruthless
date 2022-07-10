3f1c706d852a94ec61c2dc25d5e9bc7e2abdffd4
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2022-07-10 16:34:56 +0200

    Upgrade to Ruthless 0.4.0 (#57)
    Closes #56

--- ruthless-plugin/src/main/java/dev/lajoscseppento/ruthless/plugin/impl/AbstractProjectPlugin.java
+++ ruthless-plugin/src/main/java/dev/lajoscseppento/ruthless/plugin/impl/AbstractProjectPlugin.java
@@ -66,7 +66,11 @@ public abstract class AbstractProjectPlugin implements Plugin<Project> {
     apply();
   }
 
-  /** @return list of plugins which should be applied before executing this plugin's logic */
+  /**
+   * Returns the list of plugins which should be applied before executing this plugin's logic.
+   *
+   * @return the list of plugins which should be applied before executing this plugin's logic
+   */
   protected List<Class<? extends Plugin<Project>>> requiredPlugins() {
     return Collections.emptyList();
   }
