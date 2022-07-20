1dd38baf9e54a6ab11ac847dd7ab5655319ef666
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2022-07-21 01:12:15 +0200

    Use io.github.gradle-nexus.publish-plugin for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,27 @@
 plugins {
     id("org.sonarqube") version "3.4.0.2513"
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
+if (hasProperty("ossrhUsername")) {
+    nexusPublishing {
+        repositories {
+            sonatype {
+                val ossrhUsername: String by project
+                val ossrhPassword: String by project
+                username.set(ossrhUsername)
+                password.set(ossrhPassword)
+            }
+        }
+    }
+} else {
+    logger.warn("Configure project without OSSRH publishing")
 }
 
 allprojects {
     tasks.withType {
         val task = this
-        if (task.name == "check") {
+        if (name == "check") {
             rootProject.tasks.sonarqube { dependsOn(task) }
         }
     }
