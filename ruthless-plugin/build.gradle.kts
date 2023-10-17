f3eee69bc6439b50ed2a878353d66818e54f071e
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2023-10-17 22:00:57 +0200

    Upgrade to gradle-plugin-common 0.5.0

--- ruthless-plugin/build.gradle.kts
+++ ruthless-plugin/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
         implementation(gav)
     }
 
-    implementation("dev.lajoscseppento.gradle:gradle-plugin-common:0.4.0")
+    implementation("dev.lajoscseppento.gradle:gradle-plugin-common:0.5.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
     // #55 Direct declaration over dependency constraints to also propagate to the POM
     implementation("jakarta.annotation:jakarta.annotation-api:2.1.1")
